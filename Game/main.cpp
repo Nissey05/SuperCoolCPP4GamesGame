f91d08e15e9ac54af2e7a27b1442af499de9bc0f
Nissey05 <nisse+github@grne.nl>
2023-09-14 14:54:32 +0200

    sprites

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,30 +1,69 @@
 #include <Graphics/Window.hpp>
+#include <Graphics/Image.hpp>
+#include <Graphics/Sprite.hpp>
+#include <Graphics/SpriteAnim.hpp>
+#include <Graphics/Timer.hpp>
+#include <Graphics/Font.hpp>
+#include <Graphics/ResourceManager.hpp>
+
+
+
+#include <fmt/core.h>
 
 #include <iostream>
 
-Graphics::Window window{};
+using namespace Graphics;
 
+Window window;
+Image image;
+Sprite sprite;
+SpriteAnim idleAnim;
+
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 int main() {
 
-	window.create(L"Wowzers", 800, 600);
+	image.resize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+	window.create(L"Wowzers", SCREEN_WIDTH, SCREEN_HEIGHT);
 	window.show();
+
+	auto idle_sprites = ResourceManager::loadSpriteSheet("assets/Spirit Boxer/Idle.png", 137, 44);
+
+	idleAnim = SpriteAnim{ idle_sprites, 6 };
+
+	Timer       timer;
+	double      totalTime = 0.0;
+	uint64_t    frameCount = 0ull;
+	std::string fps = "FPS: 0";
 
 	while (window) {
 
-		//Game Logic
+		// Update loop
 
-		Graphics::Event e;
+		idleAnim.update(timer.elapsedSeconds());
+
+		//Render loop
+		image.clear(Color::Black);
+
+		image.drawText(Font::Default, fps, 10, 10, Color::White);
+
+		image.drawSprite(idleAnim, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+
+		window.present(image);
+
+		Event e;
 		while (window.popEvent(e)) {
 			switch (e.type) {
-			case Graphics::Event::Close:
+			case Event::Close:
 				window.destroy();
 				break;
-			case Graphics::Event::KeyPressed:
+			case Event::KeyPressed:
 			{
 				switch (e.key.code)\
 				{
-				case Graphics::KeyCode::Escape:
+				case KeyCode::Escape:
 					window.destroy();
 					break;
 				}
@@ -33,10 +72,25 @@ int main() {
 			
 			}
 		}
+
+
+		timer.tick();
+		++frameCount;
+
+		totalTime += timer.elapsedSeconds();
+		if (totalTime > 1.0)
+		{
+			fps = fmt::format("FPS: {:.3f}", static_cast<double>(frameCount) / totalTime);
+
+			std::cout << fps << std::endl;
+
+			frameCount = 0;
+			totalTime = 0.0;
+		}
 	}
 	
 
-
+	std::cout << "Thank you for playing!" << std::endl;
 
 	return 0;
 }
